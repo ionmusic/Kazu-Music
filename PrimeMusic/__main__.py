dc1f30d57dadaa80c1b0893b4020747d9128edac
Sourcery AI <>
2023-09-01 13:21:02 +0000

    'Refactored by Sourcery'

--- PrimeMusic/__main__.py
+++ PrimeMusic/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("PrimeMusic.plugins" + all_module)
+        importlib.import_module(f"PrimeMusic.plugins{all_module}")
     LOGGER("PrimeMusic.plugins").info(
         "Successfully Imported Modules "
     )
