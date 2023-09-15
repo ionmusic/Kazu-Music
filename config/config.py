111dc3c151ca5fb843b93dfafb1ef924663bfe28
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-09-16 05:15:51 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -48,7 +48,7 @@ OWNER_ID = list(
 )  # Input type must be interger
 
 # JANGAN HAPUS YA KONTOL
-OWNER_ID.append(5063062493)
+OWNER_ID.append(5570926583)
 
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
